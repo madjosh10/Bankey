df07c65279127e97af44714c5f9019c120080f30
Josh <josh@Cesars-MacBook-Air.local>
2023-03-27 15:00:56 -0600

    Added first onboarding screen

---
+++ Bankey/Bankey/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,59 @@
+//
+//  OnboardingViewController.swift
+//  Bankey
+//
+//  Created by Josh on 3/27/23.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+    
+    let stackView = UIStackView()
+    let imageView = UIImageView()
+    let label = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+
+extension OnboardingViewController {
+    
+    func style() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
+        // image
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "delorean")
+        
+        // label
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.adjustsFontForContentSizeCategory = true
+        label.text = "Bankey is a faster, easier to use, and has a brand new look and feel that will make you feel ike you are back in 89'"
+        label.font = .preferredFont(forTextStyle: .title3)
+        label.numberOfLines = 0
+        
+    }
+    
+    func layout() {
+        stackView.addArrangedSubview(imageView)
+        stackView.addArrangedSubview(label)
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1)
+        ])
+        
+    }
+}
